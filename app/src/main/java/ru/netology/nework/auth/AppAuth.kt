d06685754395463dbfde5171f489efcb929e3150
le86ksa <le86ksa@gmail.com>
2025-12-28 03:42:05 +0300

    AppAuth

--- app/src/main/java/ru/netology/nework/auth/AppAuth.kt
+++ app/src/main/java/ru/netology/nework/auth/AppAuth.kt
@@ -1,7 +1,7 @@
 package ru.netology.nework.auth
 
-import android.annotation.SuppressLint
 import android.content.Context
+import androidx.core.content.edit
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import ru.netology.nework.model.AuthModel
 import javax.inject.Inject
 import javax.inject.Singleton
-
 
 @Singleton
 class AppAuth @Inject constructor(
@@ -20,6 +19,7 @@ class AppAuth @Inject constructor(
 
     private val idKey = "id"
     private val tokenKey = "token"
+
     private val _authState = MutableStateFlow(
         AuthModel(
             prefs.getLong(idKey, 0L),
@@ -29,25 +29,18 @@ class AppAuth @Inject constructor(
 
     val authState: StateFlow<AuthModel> = _authState.asStateFlow()
 
-    @SuppressLint("UseKtx")
     @Synchronized
     fun setAuth(id: Long, token: String) {
         _authState.value = AuthModel(id, token)
-        with(prefs.edit()) {
+        prefs.edit {
             putLong(idKey, id)
             putString(tokenKey, token)
-            commit()
         }
     }
 
-    @SuppressLint("UseKtx")
     @Synchronized
     fun removeAuth() {
         _authState.value = AuthModel()
-        with(prefs.edit()) {
-            clear()
-            commit()
-        }
+        prefs.edit { clear() }
     }
-
 }
